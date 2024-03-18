7ba842711403bc0d24ac3b7554fe8f18b19e4656
Harshadsonar <Sonarharshad02@gmail.com>
2024-03-18 15:13:12 +0530

    done:

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import img1 from "../components/assets/img/ram-murti-ayodhya-hd-photo3-690x1536.jpeg";
 import img2 from "../components/assets/img/img1.jpg";
 import 'animate.css';
+import { NavBar } from "./NavBar";
 
 export default function Home() {
   
   return (
     <section id='home'>
+      <NavBar />
       <div className="banner">
         <img className="banner-img2" src={img2} alt="Ram Mandir" />
         <div>
