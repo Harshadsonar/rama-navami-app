ef223fcceca991cf24ab1ffe02fd7cfb6856ef1e
Harshadsonar <Sonarharshad02@gmail.com>
2024-03-23 15:44:25 +0530

    day changes

--- src/components/Dates.jsx
+++ src/components/Dates.jsx
@@ -5,7 +5,7 @@ export default function Dates() {
     <section className="dates-page" id='dates'>
       <h2 className="title">IMPORTANT INFO</h2>
       <div className="date-info">
-        <p className="date">Last Date of Submission is <b>12<sup>th</sup> April 2024</b></p>
+        <p className="date">Last Date of Submission is <b>Friday, 12<sup>th</sup> April 2024</b></p>
         <p><b>Languages</b></p>
         <ul style={{color:"#000", listStyleType:"none"}}>
           <li>English</li>
